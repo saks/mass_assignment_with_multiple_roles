c63b46f7229b1fc38194dab1877fb326352ce11c
saksmlz <saksmlz@gmail.com>
2012-05-30 20:44:05 +0300

    refactoring

--- lib/mass_assignment_with_multiple_roles/mass_assignment_security.rb
+++ lib/mass_assignment_with_multiple_roles/mass_assignment_security.rb
@@ -6,43 +6,52 @@ module ActiveModel
     def mass_assignment_authorizer(roles)
       active_authorizer = self.class.active_authorizer
 
-      roles_array = if roles.is_a? Array
-        roles
-      elsif roles.is_a?(String) or roles.is_a?(Symbol)
-        [roles]
-      elsif roles.respond_to? :roles
-        roles.send :roles
-      end
+      composite_role_name, roles_array = compile_role_name roles, active_authorizer
 
-      if roles_array
-        roles_array = roles_array.compact.map(&:to_sym)
-        mixed_role_name = roles_array.sort.join('_').to_sym
-      end
+      if !composite_role_name
+        active_authorizer[:default]
 
-      if roles_array and active_authorizer.has_key?(mixed_role_name)
-        active_authorizer[mixed_role_name]
-
-      elsif roles_array and has_attr_configs_for?(roles_array)
-        build_new_attrs_config roles_array, mixed_role_name
-        self.class.active_authorizer[mixed_role_name]
+      elsif active_authorizer.has_key? composite_role_name
+        active_authorizer[composite_role_name]
 
       else
-        active_authorizer[:default]
+        build_new_attrs_config roles_array, composite_role_name
+        self.class.active_authorizer[composite_role_name]
       end
     end
 
     private
 
-    def has_attr_configs_for?(roles_array)
-      roles_array.all? { |role_name| self.class.active_authorizer.has_key? role_name }
+    def compile_role_name(roles, active_authorizer)
+      roles_array = if roles.is_a? Array
+        roles
+      elsif roles.respond_to? :roles
+        [roles.send(:roles)].flatten
+      else
+        [roles]
+      end
+
+      roles_array = roles_array.find_all { |role| active_authorizer.has_key? role }
+
+      if roles_array.any?
+        [ roles_array.map(&:to_s).join('_').to_sym, roles_array ]
+      else
+        [ nil, roles_array ]
+      end
     end
 
     def build_new_attrs_config(roles_array, new_role_name)
       fields = roles_array.inject([]) do |result, role_name|
-        result += self.class._accessible_attributes[role_name].to_a
+        result += self.class.active_authorizer[role_name].to_a
       end
 
-      self.class.attr_accessible(*fields, :as => new_role_name)
+      method_name = if self.class.active_authorizer[ roles_array[0] ].is_a? ActiveModel::MassAssignmentSecurity::WhiteList
+        :attr_accessible
+      else
+        :attr_protected
+      end
+
+      self.class.send method_name, *fields, :as => new_role_name
     end
   end
 end

d4b76478f5366423d1b2e1aa0a5453b293deee39
saksmlz <saksmlz@gmail.com>
2012-05-31 00:51:31 +0300

    code reorganized

--- lib/mass_assignment_with_multiple_roles.rb
+++ lib/mass_assignment_with_multiple_roles.rb
@@ -1,6 +1,11 @@
+require 'active_model'
 require 'mass_assignment_with_multiple_roles/version'
-require 'mass_assignment_with_multiple_roles/mass_assignment_security'
+require 'mass_assignment_with_multiple_roles/active_model/mass_assignment_security'
 
-module MassAssignmentWithMultipleRoles
-  # Your code goes here...
-end
+ActiveModel::MassAssignmentSecurity.send(
+  :alias_method, :_original_mass_assignment_authorizer, :mass_assignment_authorizer)
+
+ActiveModel::MassAssignmentSecurity.send :remove_method, :mass_assignment_authorizer
+
+ActiveModel::MassAssignmentSecurity.send :include,
+  MassAssignmentWithMultipleRoles::ActiveModel::MassAssignmentSecurity
--- test/cases/helper.rb
+++ test/cases/helper.rb
@@ -1,14 +1,7 @@
-# require File.expand_path('../../../../load_paths', __FILE__)
-
 lib = File.expand_path("#{File.dirname(__FILE__)}/../../lib")
 $:.unshift(lib) unless $:.include?('lib') || $:.include?(lib)
 
-require 'config'
-require 'active_model'
-require 'active_support/core_ext/string/access'
 require 'mass_assignment_with_multiple_roles'
-require 'active_model/mass_assignment_security/permission_set'
-require 'active_model/mass_assignment_security/sanitizer'
 
 # Show backtraces for deprecated behavior for quicker cleanup.
 ActiveSupport::Deprecation.debug = true

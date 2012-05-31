1374b29c01551b673f809e919de644a07517f66a
saksmlz <saksmlz@gmail.com>
2012-05-31 10:04:52 +0300

    util methods moved from ActiveModel namespace

--- lib/mass_assignment_with_multiple_roles.rb
+++ lib/mass_assignment_with_multiple_roles.rb
@@ -3,6 +3,40 @@ require 'mass_assignment_with_multiple_roles/version'
 
 module MassAssignmentWithMultipleRoles
   ROLE_NAMES_METHOD = :role_names
+
+  def compile_role_name(roles, active_authorizer)
+    roles_array = if roles.is_a? Array
+      roles
+    elsif roles.respond_to? ROLE_NAMES_METHOD
+      [roles.send(ROLE_NAMES_METHOD)].flatten
+    else
+      [roles]
+    end
+
+    roles_array = roles_array.find_all { |role| active_authorizer.has_key? role }
+
+    if roles_array.any?
+      [ roles_array.map(&:to_s).join('_').to_sym, roles_array ]
+    else
+      [ nil, roles_array ]
+    end
+  end
+
+  def build_new_attrs_config(roles_array, new_role_name, klass)
+    fields = roles_array.inject([]) do |result, role_name|
+      result += klass.active_authorizer[role_name].to_a
+    end
+
+    method_name = if klass.active_authorizer[ roles_array[0] ].is_a? ActiveModel::MassAssignmentSecurity::WhiteList
+      :attr_accessible
+    else
+      :attr_protected
+    end
+
+    klass.send method_name, *fields, :as => new_role_name
+  end
+
+  extend self
 end
 
 module ActiveModel
@@ -13,7 +47,7 @@ module ActiveModel
     def mass_assignment_authorizer(roles)
       active_authorizer = self.class.active_authorizer
 
-      composite_role_name, roles_array = compile_role_name roles, active_authorizer
+      composite_role_name, roles_array = MassAssignmentWithMultipleRoles::compile_role_name roles, active_authorizer
 
       if !composite_role_name
         active_authorizer[:default]
@@ -22,43 +56,10 @@ module ActiveModel
         active_authorizer[composite_role_name]
 
       else
-        build_new_attrs_config roles_array, composite_role_name
+        MassAssignmentWithMultipleRoles::build_new_attrs_config roles_array, composite_role_name, self.class
         self.class.active_authorizer[composite_role_name]
       end
     end
 
-    private
-
-    def compile_role_name(roles, active_authorizer)
-      roles_array = if roles.is_a? Array
-        roles
-      elsif roles.respond_to? MassAssignmentWithMultipleRoles::ROLE_NAMES_METHOD
-        [roles.send(MassAssignmentWithMultipleRoles::ROLE_NAMES_METHOD)].flatten
-      else
-        [roles]
-      end
-
-      roles_array = roles_array.find_all { |role| active_authorizer.has_key? role }
-
-      if roles_array.any?
-        [ roles_array.map(&:to_s).join('_').to_sym, roles_array ]
-      else
-        [ nil, roles_array ]
-      end
-    end
-
-    def build_new_attrs_config(roles_array, new_role_name)
-      fields = roles_array.inject([]) do |result, role_name|
-        result += self.class.active_authorizer[role_name].to_a
-      end
-
-      method_name = if self.class.active_authorizer[ roles_array[0] ].is_a? ActiveModel::MassAssignmentSecurity::WhiteList
-        :attr_accessible
-      else
-        :attr_protected
-      end
-
-      self.class.send method_name, *fields, :as => new_role_name
-    end
   end
 end
